dc620dbc4d8b9f6436fc2fd3157bdb693d00adbd
1986700345 <1986700345@qq.com>
2019-02-27 21:09:31 +0800

    第二次提交

---
+++ src/script/js/cart.js
@@ -0,0 +1,182 @@
+!function(){
+	//渲染商品列表，传入两个参数，id和数量
+	function goodslist(id,count){
+		$.ajax({
+			url:"../php/data.php",
+			dataType:'json'
+		}).done(function(data){
+			$.each(data, function(index,value) {
+				if(id==value.sid){//遍历判断sid和传入的sid是否相同，方便将那条数据设置到渲染的商品列表中。
+					var $clonebox=$('.view-cartlist:hidden').clone(true,true);
+					$clonebox.find('.image').find('img').attr('src',value.url);
+					$clonebox.find('.image').find('img').attr('sid',value.sid);
+					$clonebox.find('.title').html(value.title);
+					$clonebox.find('.b-price').html(value.price);
+					$clonebox.find('.count span').html(value.price);
+					$clonebox.find('.oldprice').html(value.oldprice);
+					$clonebox.find('.quantity-form2').find('input').val(count);
+					//计算每个商品的价格
+					$clonebox.find('.b-sum').html((value.price*count).toFixed(2));
+					$clonebox.css('display','block');
+					$('.cart-list').append($clonebox);
+					priceall();//计算总价
+				}
+			});
+		});
+	}
+	//获取cookie，执行渲染列表的函数
+	if(getcookie('cookiesid') && getcookie('cookienum')){
+		var s=getcookie('cookiesid').split(',');//数组sid
+		var n=getcookie('cookienum').split(',');//数组num
+		$.each(s,function(i,value){
+			goodslist(s[i],n[i]);
+		});
+	}
+	//如果购物车为空，显示cart-empty盒子
+	kong();
+	function kong(){
+		if(getcookie('cookiesid') && getcookie('cookienum')){
+			$('.cart-list').show();//cookie存在，购物车有商品，隐藏盒子。
+			$('.cart-empty').hide()
+		}else{
+			$('.cart-empty').show();
+			$(".cart-list").hide();
+		}
+	}
+	//计算总价和总的商品件数，必须是选中的商品。
+	function priceall(){
+		var $sum=0;
+		var $count=0;
+		$('.view-cartlist:visible').each(function(index,element){
+			if($(element).find('.cart-checkbox input').prop('checked')){
+				$sum+=parseInt($(element).find('.quantity-form2').find('input').val());
+				$count+=parseFloat($(element).find('.b-sum').html());
+			}
+		});
+		$('.count-num').find('span').html($sum);
+		$('.count-pay').html('￥'+$count.toFixed(2));
+	}
+	//全选操作
+	$('#quanxuan').on('change',function(){
+		$('.view-cartlist:visible').find(':checkbox').prop('checked',$(this).prop('checked'));
+		$('#quanxuan').prop('checked',$(this).prop('checked'));
+		priceall();//取消选项，重算总和。
+	});
+	var $inputs=$('.view-cartlist:visible').find(':checkbox');
+	$('.cart-list').on('change',$inputs,function(){//事件的委托的this指向被委托的元素
+		if($('.view-cartlist:visible').find('input:checkbox').length==$('.view-cartlist:visible').find('input:checked').size()){
+			$('#quanxuan').prop('checked',true);
+		}else{
+			$('#quanxuan').prop('checked',false);
+		}
+		priceall();//取消选项，重算总和。
+	});
+	//数量的改变
+    //改变商品数量++
+    $('.add-num').on('click',function(){
+    	var $count=$(this).parents('.view-cartlist').find('.quantity-form2 input').val();
+    	$count++;
+    	if($count>=10){
+    		$count=10;
+    	}
+    	$(this).parents('.view-cartlist').find('.quantity-form2 input').val($count);
+    	$(this).parents('.view-cartlist').find('.b-sum').html(singlegoodsprice($(this)));//改变后的价格
+    	$(this).parents('.view-cartlist').find('.count span').html(singlegoodsprice($(this)));
+    	priceall();
+    	setcookie($(this));
+    });
+    //改变数量--
+    $('.del-num').on('click',function(){
+    	var $count=$(this).parents('.view-cartlist').find('.quantity-form2 input').val();
+    	$count--;
+    	if($count<=1){
+    		$count=1;
+    	}
+    	$(this).parents('.view-cartlist').find('.quantity-form2 input').val($count);
+    	$(this).parents('.view-cartlist').find('.b-sum').html(singlegoodsprice($(this)));//改变后的价格
+    	$(this).parents('.view-cartlist').find('.count span').html(singlegoodsprice($(this)));
+    	priceall();
+    	setcookie($(this));
+    });
+    //直接改变数量
+    $('.quantity-form2 input').on('input',function(){
+    	var $reg=/^\d+$/g;//只输入数字
+    	var $value=parseInt($(this).val());
+    	if($reg.test($value)){
+    		if($value>=10){
+    			$(this).val(10);
+    		}else if($value<=0){
+    			$(this).val(1);
+    		}else{
+    			$(this).val($value);
+    		}
+    	}else{
+    		$(this).val(1);
+    	}
+    	$(this).parents('.view-cartlist').find('.b-sum').html(singlegoodsprice($(this)));
+    	$(this).parents('.view-cartlist').find('.count span').html(singlegoodsprice($(this)));
+    	priceall();
+    	setcookie($(this));
+    });
+    //计算数量改变后的价格
+    function singlegoodsprice(obj) { //obj:当前元素
+    	var $dj=parseFloat(obj.parents('.view-cartlist').find('.b-price').html());
+    	var $cnum=parseInt(obj.parents('.view-cartlist').find('.quantity-form2 input').val())
+    	return ($dj*$cnum).toFixed(2);
+    }
+    //将改变后的数量存放到cookie
+    var arrsid=[];//商品的id
+    var arrnum=[];//商品的数量
+    //提前获取cookie里面的id和num
+    function cookietoarray(){
+		if(getcookie('cookiesid') && getcookie('cookienum')){
+			arrsid=getcookie('cookiesid').split(',');//cookie商品的sid  
+			arrnum=getcookie('cookienum').split(',');//cookie商品的num
+		}
+	}
+    function setcookie(obj){
+    	var $index=obj.parents('.view-cartlist').find('img').attr('sid');
+    	arrnum[$.inArray($index,arrsid)]=obj.parents('.view-cartlist').find('.quantity-form2 input').val();
+    	addcookie('cookienum',arrnum.toString(),7);
+    }
+    //删除操作
+    //删除cookie
+    function delgoodslist(sid,arrsid){//sid：当前删除的sid，arrsid:cookie的sid的值
+    	var $index=-1;
+    	$.each(arrsid, function(index,value) {//删除的sid对应的索引位置。 index:数组项的索引
+    		if(sid==value){
+    			$index=index;//如果传入的值和数组的值相同，返回值对应的索引。
+    		}
+    	});
+    	arrsid.splice($index, 1);//删除数组对应的值
+	    arrnum.splice($index, 1);//删除数组对应的值
+	    addcookie('cookiesid', arrsid.toString(), 7);//添加cookie
+	    addcookie('cookienum', arrnum.toString(), 7);//添加cookie
+	    
+    }
+    //删除单个商品的函数（委托）
+    $('.cart-list').on('click','.del',function(ev){
+    	cookietoarray();//得到数组,上面的删除cookie需要。
+    	 if(confirm('你确定要删除吗？')){
+	     	$(this).first().parents('.view-cartlist').remove();//通过当前点击的元素找到整个一行列表，删除
+	    }
+	    delgoodslist($(this).first().parents('.view-cartlist').find('img').attr('sid'), arrsid);
+	    priceall();
+    });
+    //删除全部商品的函数
+    $('.del-shop a').on('click',function(){
+    	cookietoarray();//得到数组,上面的删除cookie需要。
+		if(confirm('你确定要全部删除吗？')){
+		    $('.view-cartlist:visible').each(function() {
+		        if ($(this).find('input:checkbox').is(':checked')) {//复选框一定是选中的
+		            $(this).remove();
+		            delgoodslist($(this).find('img').attr('sid'), arrsid);
+		        }
+		        
+		    });
+		    priceall();
+		    $('.cart-empty').show();
+			$(".cart-list").hide();
+		}
+    });
+}();
--- src/script/js/detail.js
+++ src/script/js/detail.js
@@ -1,20 +1,89 @@
-var picsid=location.search.substring(1).split('=')[1];
-var oBox=document.querySelector('.box');
-ajax({
-	url:'http://10.31.162.36/1810/Day%2024/detail/php/details.php',
-	data:{
-		sid:picsid
-	},
-	success:function(data){
-		//console.log(JSON.parse(data));
-		var dataobj=JSON.parse(data);
-		oBox.innerHTML=`
-			<div class="box_left"><img src="${dataobj.url}" alt="" sid="${dataobj.sid}" /></div>
-			<div class="box_right">
-				<h3>${dataobj.title}</h3>
-				<span>¥${dataobj.price}</span>
-				<button>加入购物车</button>
-			</div>
-		`;
-	}
-});
+!function(){
+	//获取sid
+	var picsid=location.search.substring(1).split('=')[1];
+	//将当前的id传给后端获取对应数据
+	$.ajax({
+		url:"../php/details.php",
+		data:{
+			sid:picsid
+		},
+		dataType:'json'
+	}).done(function(data){//data:后端返回的和id对应的数据
+		$("#spic img").attr('src',data.url);
+		$("#bf img").attr("src",data.url);
+		$("#spic img").attr("sid",data.url);
+		$(".font-style .title").html(data.title);
+		$(".now-price .n-price span").html(data.price);
+		$(".past-price del").html(data.oldprice);
+		$(".discount-price strong").html(data.discount);
+		$(".btn .price-wrap span").html(data.price);
+		var arr=data.urls.split(",");
+		var str='';
+		$.each(arr, function(index,value) {
+			str+='<li><img src="'+value+'"/></li>';
+		});
+		$("#list ul").html(str);
+	});
+	//放大镜效果
+	!function(){
+		$("#sf").width($('#spic').width()*$("#bf").width()/$("#bf img").width());
+		$("#sf").height($('#spic').height()*$("#bf").height()/$("#bf img").height());
+		var bili=$('#bf img').width()/$("#spic").width();
+		$("#spic").hover(function(){
+			$("#sf").css('visibility','visible');
+			$("#bf").css("visibility","visible");
+			$(this).on('mousemove',function(ev){
+				var $left=ev.pageX-$("#spic").offset().left-$("#sf").width()/2;
+				var $top=ev.pageY-$("#spic").offset().top-$("#sf").height()/2;
+				if($left<0){
+					$left=0;
+				}else if($left>=$('#spic').width()-$('#sf').width()){
+					$left=$('#spic').width()-$('#sf').width();
+				}
+				if($top<0){
+					$top=0;
+				}else if($top>=$('#spic').height()-$('#sf').height()){
+					$top=$('#spic').height()-$('#sf').height();
+				}
+				$('#sf').css('left',$left);
+				$('#sf').css('top',$top);
+				$('#bf img').css('left',-$left*bili);
+				$('#bf img').css('top',-$top*bili);
+			});
+		},function(){
+			$('#sf').css('visibility','hidden');
+			$('#bf').css('visibility','hidden');
+		});
+		//点击小图切换
+		$('#list ul').on('click','li',function(){
+			var $imgurl=$(this).find('img').attr('src');
+			$("#spic img").attr('src',$imgurl);
+			$('#bf img').attr('src',$imgurl);
+		});
+	}();
+	//提前获取cookie里面id和num
+   //点击按钮将商品的数量和id存放cookie中
+   var arrsid=[];//商品的sid
+   var arrnum=[];//商品的数量
+   function cookietoarray(){
+   	if(getcookie('cookiesid') && getcookie('cookienum')){
+   		arrsid=getcookie('cookiesid').split(',');//cookie商品的sid
+   		arrnum=getcookie('cookienum').split(',');//cookie商品的num
+   	}
+   }
+   $('.view-left .btn').on('click',function(){//点击加入购物车
+   		var $sid=picsid
+   		cookietoarray();//获取已经存在的cookie值
+   		if($.inArray($sid,arrsid)!=-1){//商品存在，数量叠加
+   			//先取出cookie中的对应的数量值+当前添加的数量值，添加到对应的cookie中。
+   			var num=parseInt(arrnum[$.inArray($sid,arrsid)])+parseInt($("#count").val());
+   			arrnum[$.inArray($sid,arrsid)]=num;
+   			addcookie('cookienum',arrnum.toString(),10);//数组存入cookie
+   		}else{//不存在，第一次添加。将商品的id和数量存入数组，再存入cookie.
+   			arrsid.push($sid);//将当前的id存入数组
+   			addcookie('cookiesid',arrsid.toString(),10);//数组存入cookie
+   			arrnum.push($('#count').val());
+   			addcookie('cookienum',arrnum.toString(),10);//数组存入cookie
+   		}
+   });
+}();
